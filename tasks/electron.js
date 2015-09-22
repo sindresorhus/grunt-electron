3f8252903f43dae936052098b61b31b3dfabae38
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-22 14:07:24 +0700

    tweaks

--- tasks/electron.js
+++ tasks/electron.js
@@ -4,12 +4,14 @@ var electronPackager = require('electron-packager');
 module.exports = function (grunt) {
 	grunt.registerMultiTask('electron', 'Package Electron apps', function () {
 		var done = this.async();
-		electronPackager(this.options(), function (error) {
-			if (error) {
-				grunt.warn(error);
-			} else {
-				done();
+
+		electronPackager(this.options(), function (err) {
+			if (err) {
+				grunt.warn(err);
+				return;
 			}
+
+			done();
 		});
 	});
 };
